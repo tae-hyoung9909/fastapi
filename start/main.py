e4013bdf3a8f1514a0476b8de00588226bf874fb
tae-hyoung9909 <taekim9909@gmail.com>
2026-01-01 17:27:05 +0900

    docs: add FastAPI run command and Swagger UI usage

--- start/main.py
+++ start/main.py
@@ -1,12 +1,16 @@
+# FastAPI Import
 from fastapi import FastAPI
 
+# Create FastAPI instance
 app = FastAPI()
 
+# Define a simple GET endpoint
+# Opertaion은 get, post, put, delete 등의 http method를 의미
 @app.get("/", summary="simple api",
+         tags=["Root Endpoint"],
          description="A simple API endpoint")
 async def root():
     '''
     A simple endpoint that returns a greeting message.
     '''
-    return {"message": "Hello World"}
-
+    return {"message": "Hello World"} # application/json으로 반환
---
+++ start/readme.md
@@ -0,0 +1,24 @@
+# FastAPI
+
+#### FastAPI 실행 방법
+```
+uvicorn main:app --reload --port 8080
+```
+* http://localhost:8080로 접근
+
+#### Swagger UI
+* Swagger UI는 API들을 브라우저 기반에서 편리하게 관리 및 문서화, 테스트 할 수 있는 기능을 제공
+
+##### 사용법
+* `/docs`로 접근
+* summary, tags, description로 문서화 가능
+* description은 ''' '''으로 마크다운으로 작성 가능
+
+```python
+async def root():
+    '''
+    이쪽 부분에 마크다운 작성
+    '''
+    return {"message": "Hello World"} # application/json으로 반환
+
+```
